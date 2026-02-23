6742c0c35755fff9880949fd4d88730adef8b5ce
Escalade <78426164+EscaladeDev@users.noreply.github.com>
2026-02-23 13:54:04 -0500

    Delete utils directory

--- utils/snap.js
+++
@@ -1,9 +0,0 @@
-export function snap(point, gridSize, strength = 0.85) {
-  const gx = Math.round(point.x / gridSize) * gridSize
-  const gy = Math.round(point.y / gridSize) * gridSize
-
-  return {
-    x: point.x * (1 - strength) + gx * strength,
-    y: point.y * (1 - strength) + gy * strength
-  }
-}
